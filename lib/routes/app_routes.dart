771ab1b8eae730f4413f23dd32932870394f68c9
Dhruv Balodia <dhruv.balodia92@gmail.com>
2026-07-05 19:24:36 +0530

    Project structure organized

---
+++ lib/routes/app_routes.dart
@@ -0,0 +1,8 @@
+class AppRoutes {
+  static const String splash = "/";
+  static const String login = "/login";
+  static const String home = "/home";
+  static const String restaurant = "/restaurant";
+  static const String cart = "/cart";
+  static const String profile = "/profile";
+}
