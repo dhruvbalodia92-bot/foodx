ca6aed554a939786e1ecaed50022f37bbce1ac6d
Dhruv Balodia <dhruv.balodia92@gmail.com>
2026-07-07 09:15:49 +0530

    Splash and Login UI completed

--- lib/screens/splash/splash_screen.dart
+++ lib/screens/splash/splash_screen.dart
@@ -1,20 +1,58 @@
 import 'package:flutter/material.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
 
   @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+
+    Future.delayed(const Duration(seconds: 3), () {
+      // Login Screen pe baad me bhejenge
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      backgroundColor: Colors.orange,
+    return Scaffold(
+      backgroundColor: const Color(0xFFFF6B00),
       body: Center(
-        child: Text(
-          "FoodX",
-          style: TextStyle(
-            fontSize: 36,
-            fontWeight: FontWeight.bold,
-            color: Colors.white,
-          ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const [
+            Icon(
+              Icons.fastfood,
+              color: Colors.white,
+              size: 90,
+            ),
+
+            SizedBox(height: 20),
+
+            Text(
+              "FoodX",
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 42,
+                fontWeight: FontWeight.bold,
+                letterSpacing: 2,
+              ),
+            ),
+
+            SizedBox(height: 10),
+
+            Text(
+              "Fast • Fresh • Delivered",
+              style: TextStyle(
+                color: Colors.white70,
+                fontSize: 16,
+              ),
+            ),
+          ],
         ),
       ),
     );
